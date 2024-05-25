62f2fe108837e8c8a75c86d480069cde0c9ce6fe
Sara Romao <sara-romao@live.com>
2024-05-25 10:22:20 -0300

    feat:exer de horas

--- src/pages/Horas/1-exerc-horas.tsx
+++ src/pages/Horas/1-exerc-horas.tsx
@@ -1,9 +1,34 @@
 
 export const Exer1Horas = () =>{
 
+     const fullTime = new Intl.DateTimeFormat('pt-BR', {
+       timeStyle: 'short',
+       hour12: false
+
+     }).format();
+
+     const hour = new Date().getHours();
+     let greeting = '';
+
+     if(hour >= 0 && hour < 12){
+      greeting = "Bom dia 🌞";
+     
+     }else if(hour >= 12 && hour <= 18){
+       greeting = "Boa Tarde 🌅";
+
+     }else if(hour >= 18 && hour <= 23){
+       greeting = "Boa Noite 🌛";
+     }
+
+
     return(
   
-      <h1>Horas</h1>
+      <div className=" h-screen flex flex-col justify-center items-center text-white bg-gradient-to-r from-sky-500 to-indigo-500">
+
+         <div className="text-9xl">{fullTime}</div>
+
+         <div className="text-5xl font-bold">{greeting} </div>
+      </div>
   
   
     )
